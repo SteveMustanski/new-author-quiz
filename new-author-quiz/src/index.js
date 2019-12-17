1c038955db8370930da75796168be5243c209874
SteveMustanski <steve.mustanski@gmail.com>
2019-12-17 15:41:50 -0500

    added bootstrap and updated test

--- new-author-quiz/src/index.js
+++ new-author-quiz/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './bootstrap.min.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
 
